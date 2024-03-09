b375ec21bd4cf7e18daaa026b160cc27d3e41793
Wallysson <wally.nm@gmail.com>
2024-03-09 17:43:36 -0300

    Initial Files

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static';
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
+ 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+ 
+    kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        }
+    }
+};
+ 
+export default config;
