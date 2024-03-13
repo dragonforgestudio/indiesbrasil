0f57274ed0df7544629a1736a89848134366c09a
Wallysson <wally.nm@gmail.com>
2024-03-13 01:33:52 -0300

    Fixes base path not accepting empty

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from "mdsvex";
-const base = process.env.NODE_ENV === 'production' ? '' : ''
+const base = process.env.NODE_ENV === 'production' ? '/' : ''
  
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
